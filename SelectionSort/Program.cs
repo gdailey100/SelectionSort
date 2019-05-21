8aeb011cf439c10a7df85534c02867c4f2ccfffe
Glenn Dailey <glenndaileydj@hotmail.com>
2019-05-21 15:44:08 -0400

    Updated

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -4,20 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Written by Glenn Dailey
+
 namespace SelectionSort
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // create array size of 10
             int array_size = 10;
+            // place items in array unsorted
             int[] array = new int[10] { 100, 50, 20, 40, 10, 60, 80, 70, 90, 30 };
+            // print the array before the sort is done
             Console.WriteLine("The Array Before Selection Sort is: ");
             for (int i = 0; i < array_size; i++)
             {
                 Console.WriteLine(array[i]);
             }
 
+            // create loops, temp, min_key to sort the array
             int temp, min_key;
             for (int j = 0; j < array_size - 1; j++)
             {
@@ -34,6 +40,7 @@ namespace SelectionSort
                 array[j] = temp;
             }
 
+            // print the sorted array
             Console.WriteLine("The Array After Selection Sort is: ");
             for (int i = 0; i < 10; i++)
             {
